f54740aff7932c19e02759e591f16c9ecd4bfc92
11620340 <73938922+11620340@users.noreply.github.com>
2020-11-14 02:35:07 +0800

    Add files via upload

---
+++ c/main.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include "sum.h"
+
+
+int main()
+{
+	int x, y;
+	scanf("%d%d", &x, &y);
+	add(x, y);
+	return 0;
+}
---
+++ c/sum.h
@@ -0,0 +1,9 @@
+# include <stdio.h>
+
+int add(int a,int b)
+{
+        int c;
+        c = a + b;
+        printf("%d\n", c);
+        return 0;
+}
